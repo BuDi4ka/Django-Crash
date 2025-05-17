794f15af7163cdfffbeb9fbf808dd4f5cd363260
Dmytro Burdeniuk <dmytroburdenuk@gmail.com>
2025-05-17 12:11:55 +0100

    templates

--- crash_course/firstapp/urls.py
+++ crash_course/firstapp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('function', views.hello_world),
-    path('class', views.HelloUK.as_view())
+    path('class', views.HelloUK.as_view()), 
+    path('reservation', views.home)
 ]
 
--- crash_course/firstapp/views.py
+++ crash_course/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .forms import ReservationForm
 
 # Create your views here.
 def hello_world(request):
@@ -10,3 +11,15 @@ def hello_world(request):
 class HelloUK(View):
     def get(self, request):
         return HttpResponse('Hello UK')
+
+
+def home(request):
+    form = ReservationForm()
+
+    if request.method == 'POST':
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Success')
+        
+    return render(request, 'index.html', {'form': form })
